0fa62cccfd6ff6f9d071a105032fbdf111855add
DiogoRio <diogorio53@hotmail.com>
2021-11-07 17:17:42 +0000

    feat: init top bar according to mockups

---
+++ app/frontend/components/NavBar.js
@@ -0,0 +1,37 @@
+import { Container, Navbar, Form, FormControl, Button, InputGroup } from "react-bootstrap"
+import { BsJustify, BsPersonCircle, BsBagFill, BsSearch  } from 'react-icons/bs'
+
+import styles from "../styles/NavBar.module.css"
+
+const NavBar = () => {
+    return (
+        <Navbar className={ `primary-bg-color ${styles.navbar} `}>
+            <Container fluid className="navbarContainer">
+                <Container className="d-flex align-items-center py-0">
+                    <button className={styles.btn} >
+                        <BsJustify size={48}/>
+                    </button>
+                    <Navbar.Brand href="/" className>
+                        <h1 className="ps-2 my-0 secondary-color">Retail Marketplace</h1>
+                    </Navbar.Brand >
+                </Container>
+                <InputGroup className="d-flex flex-row">
+                    <FormControl className={styles.searchBox} placeholder="Search"/>
+                    <button className={styles.searchBtn} >
+                        <BsSearch/>
+                    </button>
+                </InputGroup>
+                <Container className="d-flex flex-row-reverse">
+                    <button className={styles.btn} >
+                        <BsBagFill size={48}/>
+                    </button>
+                    <button className={styles.btn}  >
+                        <BsPersonCircle size={48} className="me-3"/>
+                    </button>
+                </Container>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar
--- app/frontend/pages/_app.js
+++ app/frontend/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from '../components/Layout'
 
+// This wraps up every page components, so the Layout will be displayed in every page
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    )
 }
 
 export default MyApp
