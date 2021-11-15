0af596c563cfc26ab33645d6a561ffd9510ffaba
DiogoRio <diogorio53@hotmail.com>
2021-11-15 21:40:14 +0000

    feat: now the bootstrap theme can be adjusted in styles/globals.css

--- app/frontend/pages/_app.js
+++ app/frontend/pages/_app.js
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/Layout'
 
 // This wraps up every page components, so the Layout will be displayed in every page
