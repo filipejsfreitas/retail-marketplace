3ecffa20fcace5b4522f29a5eb5d541bd927df72
DiogoRio <diogorio53@hotmail.com>
2021-11-17 15:55:31 +0000

    fix: now some pages can not have top and side bars (ex. login)

--- app/frontend/components/Layout.js
+++ app/frontend/components/Layout.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useRouter } from 'next/router'
 
 import NavBar from "./NavBar"
 import SideBar from "./SideBar"
@@ -6,19 +7,29 @@ import SideBar from "./SideBar"
 
 const Layout = ({children}) => {
 
+    const router = useRouter()
+
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-
-    return (
-        <>
-        <NavBar handleShow={handleShow}/>
-        <SideBar handleClose={handleClose} show={show}/>
-        {children}
-        </>
-    )
+    // In the login page there is no NavBar or SideBar, so this components won't be rendered
+    if(router.asPath === '/login') {
+        return (
+            <>
+            {children}
+            </>
+        )
+    } else {
+        return (
+            <>
+            <NavBar handleShow={handleShow}/>
+            <SideBar handleClose={handleClose} show={show}/>
+            {children}
+            </>
+        )
+    }
 }
 
 export default Layout
