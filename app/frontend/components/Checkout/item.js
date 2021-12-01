b8d12734251f9253b3dcce3f619cd4921ddf02c8
DiogoRio <diogorio53@hotmail.com>
2021-12-01 16:26:10 +0000

    [Frontend] feat: item component that is going to be used in the Checkout component to display information about items currently in the checkout bag

---
+++ app/frontend/components/Checkout/item.js
@@ -0,0 +1,39 @@
+import { Container } from "react-bootstrap"
+import Image from "next/dist/client/image"
+
+import styles from "styles/checkout/item.module.css"
+
+
+const Item = (props) => {
+
+    const quantity = props.quantity
+    const price = props.quantity * props.price
+    const title = props.title
+
+    return (
+        <Container className="d-flex my-3">
+        <Container className={styles.itemImg}>
+            <Image
+                src="https://nayemdevs.com/wp-content/uploads/2020/03/default-product-image.png"
+                alt=""
+                width={110}
+                height={110}
+                layout="responsive"
+                />
+        </Container> 
+        <div className={styles.productInfo}>
+            <div className={styles.productTitle}> {`${title}`} </div>
+            <div className="d-flex flex-row justify-content-between">
+                <div className={styles.productQuantity}>
+                    {`Quantity: ${quantity}`} 
+                </div>
+                <div className={styles.productPrice}>
+                    {`${price}€`}
+                </div>
+            </div>
+        </div>
+    </Container>
+    )
+}
+
+export default Item
