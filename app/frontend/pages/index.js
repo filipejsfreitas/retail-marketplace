03791aa07ad53c61c0cfa07afe05b474fd684095
DiogoRio <diogorio53@hotmail.com>
2021-12-03 21:26:00 +0000

    [Frontend] feat: categories fetched from API and passed down to the SideBar component

--- app/frontend/pages/index.js
+++ app/frontend/pages/index.js
@@ -1,8 +1,30 @@
 import Layout from "components/Layout"
+import fetchCategories, { revalidateTime } from "helper/DynamicCategoriesHelper";
 
-export default function Home() {
+export default function Home({ categories }) {
   return (
-    <Layout>
+    <Layout categories={categories}>
     </Layout>
   )
+}
+
+// This function gets called at build time on server-side.
+// Next.js will attempt to re-generate the page:
+// - When a request comes in
+// - At most once every X seconds, X being the value in the 
+// revalidate const.
+// In development (npm run dev) this function is called on every 
+// request
+export async function getStaticProps() {
+  
+  const categories = await fetchCategories();
+  const revTime = revalidateTime()
+
+  return {
+    props: {
+      categories,
+    },
+
+    revalidate: revTime, 
+  }
 }
