15ddb0f3633ba1d123d30ebd58102e8f14accccf
DiogoRio <diogorio53@hotmail.com>
2021-12-03 22:11:40 +0000

    [Frontend] feat: improved sidebar visuals and it now supports displaying categories provided via props ( supports displaying the dynamic categories fetched from the backend )

--- app/frontend/components/NavBar/SideBar.js
+++ app/frontend/components/NavBar/SideBar.js
@@ -1,27 +1,35 @@
 import { Offcanvas, Container } from "react-bootstrap"
 import { BsX } from 'react-icons/bs'
+import Link from "next/link"
+
+import LogoReversed from "components/Logos/LogoReversed"
 
 import styles from 'styles/NavBar/SideBar.module.css'
 
 
-const SideBar = (props) => {
+// Receives an array of categories from the backend API
+// Returns the respective links for each of the categories
+const renderCategories = (categories) => {
+    return categories.map( ({ _id,name,parent_id,level,children },i) => 
+        <div className={styles.link} key={_id} ><Link href={`/${name}`}>{`${name}`}</Link></div> )
+}
 
+const SideBar = (props) => {
     return (
         <Offcanvas show={props.show} onHide={props.handleClose}>
             <Offcanvas.Header className="p-0">
-                <Container className={`${styles.topRec} d-flex align-items-center bg-primary`}>
+                <Container className={styles.topRec}>
                     <button className={styles.closeBtn} onClick={props.handleClose}>
                         <BsX size={48}/>
                     </button>
+                    <LogoReversed />
                 </Container>
             </Offcanvas.Header>
             <Offcanvas.Body className={styles.body}>
-                <button className={styles.button}><h4>Featured</h4></button>
-                <button className={styles.button}><h4>Recommended</h4></button>
-                <button className={styles.button}><h1>Store</h1></button>
-                <button className={styles.button}><h4>Electronics</h4></button>
-                <button className={styles.button}><h4>Home and Kitchen</h4></button>
-                <button className={styles.button}><h4>Clothing</h4></button>
+                <div className={styles.link}><Link href="/trending">Trending</Link></div>
+                <div className={styles.link}><Link href="/new">New</Link></div>
+                <div className={styles.line}></div>
+                { renderCategories(props.categories) }
             </Offcanvas.Body>
         </Offcanvas>
     )
