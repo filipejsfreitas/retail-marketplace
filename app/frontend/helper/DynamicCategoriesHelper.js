d1611e7110e89d42cd8bbbaf4d777e63b52d6486
DiogoRio <diogorio53@hotmail.com>
2021-12-04 14:28:32 +0000

    [Frontend] fix: when api return an error the dynamic categories helper now returns a set of hard-coded categories

--- app/frontend/helper/DynamicCategoriesHelper.js
+++ app/frontend/helper/DynamicCategoriesHelper.js
@@ -1,3 +1,25 @@
+// Handle function for an api error
+// in production this should return
+// well known and established categories
+function handleError(error) {
+  return [
+    {
+      _id: '61aa126fccb5ec1f90da8b04',
+      name: 'Electronics',
+      parent_id: null,
+      level: 0,
+      children: []
+    },
+    {
+      _id: '61aa1367ccb5ec1f90da8b08',
+      name: 'Computers',
+      parent_id: null,
+      level: 0,
+      children: []
+    }
+  ]
+}
+
 export function revalidateTime() {
   // This value is in seconds
   // 5 mins = 300 s
@@ -15,9 +37,9 @@ export function revalidateTime() {
 // Helper function that handles the fetch of the categories
 // currently in the database via the backend API 
 export default async function fetchCategories() {
-    const res = await fetch(`${process.env.HOST}/category/`)
-    const data = await res.json();
-    const categories = data.data;
-    
+    const categories = await fetch(`${process.env.HOST}/category/`)
+                            .then( (res) => res.json() )
+                            .then( (data) => data.data )
+                            .catch( (error) => handleError(error))
     return categories
 }
