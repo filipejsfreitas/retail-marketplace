fe511275cf2aca7ee152cca29311d87fbebfcaf4
DiogoRio <diogorio53@hotmail.com>
2021-12-21 20:49:49 +0000

    [Frontend] improv: now the UI supports subcategories; closes #41

---
+++ app/frontend/components/NavBar/SideBar/SubCategories.js
@@ -0,0 +1,37 @@
+import { Offcanvas } from "react-bootstrap"
+import { BsArrowLeft } from 'react-icons/bs'
+import Link from "next/link"
+
+import styles from 'styles/NavBar/SideBar/SubCategorie.module.css'
+
+const SubCategories = ({category,handleClose}) => {
+    
+    return (
+      <Offcanvas.Body className={styles.body}>
+        <div className={styles.categorieBox}>
+          <btn className={styles.back} onClick={() => handleClose()}>
+            {" "}
+            <BsArrowLeft size={32} />{" "}
+          </btn>
+          <span className={styles.categorie}><Link href={`/${category.name}`}>{category.name}</Link></span>
+          <div></div>
+        </div>
+        <div className={styles.line}></div>
+        {category.children.map(({ name, children }, i) => {
+            const father = name;
+            return(
+          <div key={i} className={styles.wrapperSub}>
+            <div className={styles.subCategorie}><Link href={`/${category.name}/${name}`} >{name}</Link></div>
+            <div className={styles.wrapperSubSub}>
+              {children.map(({ name }, i) => (
+                <Link href={`/${category.name}/${father}/${name}`} key={i}>{name}</Link>
+              ))}
+            </div>
+          </div>
+        )
+        })}
+      </Offcanvas.Body>
+    );
+}
+
+export default SubCategories
---
+++ app/frontend/components/NavBar/SideBar/index.js
@@ -0,0 +1,55 @@
+import { Offcanvas, Container, Button } from "react-bootstrap"
+import { BsX } from 'react-icons/bs'
+import Link from "next/link"
+
+import LogoReversed from "components/Logos/LogoReversed"
+import SubCategories from "./SubCategories"
+
+import styles from 'styles/NavBar/SideBar/SideBar.module.css'
+import { useEffect, useState } from "react"
+
+const SideBar = (props) => {
+
+    // If in a sub categorie menu categorie id else 0
+    const [subCategorie,setSubCategorie] = useState(false)
+
+    useEffect( () => {
+        console.log("CHANGED:", subCategorie)
+    }, [subCategorie])
+
+    // Receives an array of categories from the backend API
+    // Returns the respective links for each of the categories
+    const renderCategories = (categories) => {
+        return categories.map( ({ _id,name,children }) => 
+            <div className={styles.link} key={_id} ><button className={styles.link} onClick={ () => setSubCategorie({children:children,name:name}) }>{`${name}`}</button></div> )
+    }
+
+    return (
+      <Offcanvas show={props.show} onHide={props.handleClose} onExited={ () => setSubCategorie(false)}>
+        <Offcanvas.Header className="p-0">
+          <Container className={styles.topRec}>
+            <button className={styles.closeBtn} onClick={props.handleClose}>
+              <BsX size={48} />
+            </button>
+            <LogoReversed />
+          </Container>
+        </Offcanvas.Header>
+        {subCategorie === false ? (
+          <Offcanvas.Body className={styles.body}>
+            <div className={styles.link}>
+              <Link href="/trending">Trending</Link>
+            </div>
+            <div className={styles.link}>
+              <Link href="/new">New</Link>
+            </div>
+            <div className={styles.line}></div>
+            {renderCategories(props.categories)}
+          </Offcanvas.Body>
+        ) : (
+          <SubCategories category={subCategorie} handleClose={ () => setSubCategorie(false) }/>
+        )}
+      </Offcanvas>
+    );
+}
+
+export default SideBar
