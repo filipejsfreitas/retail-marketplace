d63edb33bfec6e71c64c4c3cf7922c403754df0a
Kaixi26 <jorgejg575926@gmail.com>
2021-12-27 00:37:29 +0000

    [Frontend] - better responsiveness on register page

--- app/frontend/pages/register/index.js
+++ app/frontend/pages/register/index.js
@@ -1,32 +1,40 @@
-import { Button } from "react-bootstrap"
-import { useRouter } from "next/router"
+import Link from 'next/link'
+import { useState } from "react"
 
 import Logo from "components/Logos/Logo"
 import styles from "styles/register/root.module.css"
 
 export default function Root() {
 
-    const router = useRouter()
+    const [hoveredClient, setHoveredClient] = useState(false)
+    const [hoveredSeller, setHoveredSeller] = useState(false)
 
     return (
         <div className={styles.wrapper}>
             <div className={styles.logo}>
-                <Logo/>
+                <Logo />
             </div>
             <div className={styles.registerOpt}>
-                <div className={styles.client}>
-                    <span>You are a customer that</span>
-                    <span>wants to buy in Retail </span>
-                    <span>Marketplace</span>
-                    <Button onClick={ () => router.push('/register/client') } className={styles.btn} variant="secondary">Register</Button>
-                </div>
+                <Link href="/register/client">
+                    <a className={`${styles.client} ${hoveredSeller ? styles.grayscale : ""}`}
+                        onMouseLeave={() => setHoveredClient(false)}
+                        onMouseEnter={() => setHoveredClient(true)} >
+                        <span>You are a customer that</span>
+                        <span>wants to buy in Retail </span>
+                        <span>Marketplace</span>
+                    </a>
+                </Link>
                 <div className={styles.line}></div>
-                <div className={styles.seller}>
-                    <span>You are a company that</span>
-                    <span>wants to sell in Retail </span>
-                    <span>Marketplace</span>
-                    <Button onClick={ () => router.push('/register/seller') } className={styles.btn} variant="secondary">Register</Button>
-                </div>
+                <Link href="/register/seller">
+                    <a className={`${styles.seller} ${hoveredClient ? styles.grayscale : ""}`}
+                        onMouseLeave={() => setHoveredSeller(false)}
+                        onMouseEnter={() => setHoveredSeller(true)}
+                    >
+                        <span>You are a company that</span>
+                        <span>wants to sell in Retail </span>
+                        <span>Marketplace</span>
+                    </a>
+                </Link>
             </div>
         </div>
     )
