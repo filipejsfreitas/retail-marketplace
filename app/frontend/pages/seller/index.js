b8185e56e5fd1f6704d84a6f12912d5c3f2b691d
Kaixi26 <jorgejg575926@gmail.com>
2022-01-06 01:45:11 +0000

    [Frontend] - Pending orders pie chart

--- app/frontend/pages/seller/index.js
+++ app/frontend/pages/seller/index.js
@@ -6,10 +6,11 @@ import { useState, useEffect } from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { ResponsivePie } from '@nivo/pie'
 
 import styles from "styles/Seller/index.module.css"
 
-function DefaultCarousel({children, ...props}) {
+function DefaultCarousel({ children, ...props }) {
   const responsive = {
     desktop: { breakpoint: { max: 3000, min: 1024 }, items: 1, slidesToSlide: 1 }
   }
@@ -38,23 +39,72 @@ function RecommendedCategories({ recommendedCategories }) {
   return <div>
     <h5>Recommended Categories</h5>
     <div className={styles.panel_cat}>
-    <DefaultCarousel>
-      {
-        recommendedCategories.categories.map(cat => (
-          <div key={cat} className={styles.carr_cat_elem}>
-            {cat}
-          </div>
-        ))}
+      <DefaultCarousel>
+        {
+          recommendedCategories.categories.map(cat => (
+            <div key={cat} className={styles.carr_cat_elem}>
+              {cat}
+            </div>
+          ))}
       </DefaultCarousel>
+    </div>
   </div>
-  </div>
+}
+
+function OrdersPie({ ordersOverview }) {
+  const data = [
+    { "id": "processing", "label": "Processing", "value": ordersOverview.processing },
+    { "id": "sent", "label": "Processing", "value": ordersOverview.sent + 1 },
+  ]
+  return <div style={{ height: 300 }}>
+    <ResponsivePie
+      data={data}
+      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      innerRadius={0.5}
+      padAngle={2}
+      cornerRadius={3}
+      activeOuterRadiusOffset={8}
+      borderWidth={1}
+      borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+      arcLinkLabelsSkipAngle={10}
+      arcLinkLabelsTextColor="#333333"
+      arcLinkLabelsThickness={2}
+      arcLinkLabelsColor={{ from: 'color' }}
+      arcLabelsSkipAngle={10}
+      arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
+      legends={[
+        {
+          anchor: 'bottom',
+          direction: 'row',
+          justify: false,
+          translateX: 0,
+          translateY: 56,
+          itemsSpacing: 0,
+          itemWidth: 100,
+          itemHeight: 18,
+          itemTextColor: '#999',
+          itemDirection: 'left-to-right',
+          itemOpacity: 1,
+          symbolSize: 18,
+          symbolShape: 'circle',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemTextColor: '#000'
+              }
+            }
+          ]
+        }
+      ]}
+    />
+  </div >
 }
 
 export default function Home() {
   const { fetchAuth: fetch } = useFetchAuth()
   const { isReady } = useRouter()
   const [panel, setPanel] = useState(undefined)
-  console.debug(panel)
 
   useEffect(() => {
     if (!isReady) return
@@ -62,11 +112,11 @@ export default function Home() {
       .then(rep => rep.json())
       .then(p => setPanel(p))
   }, [isReady])
-  //console.debug(panel)
 
   return <Layout sidebar={SELLER_SIDEBAR} isLoading={panel === undefined}>
     {panel && <>
-      <RecommendedCategories recommendedCategories={panel.recommendedCategories} />
+      {/*<RecommendedCategories recommendedCategories={panel.recommendedCategories} />*/}
+      <OrdersPie data={data} ordersOverview={panel.ordersOverview} />
     </>}
   </Layout>
 }
