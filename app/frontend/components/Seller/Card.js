7ebe2e6d8fc4c0164741a414390ca0172694181c
Kaixi26 <jorgejg575926@gmail.com>
2022-01-18 20:06:24 +0000

    [Frontend] - small cards added

--- app/frontend/components/Seller/Card.js
+++ app/frontend/components/Seller/Card.js
@@ -1,5 +1,45 @@
 import { Card } from "react-bootstrap"
 
+import { BsArrowUp, BsArrowDown, BsArrowUpShort, BsArrowDownShort } from "react-icons/bs"
+import styles from "styles/Seller/card.module.css"
+
+export function SimpleCard({ title, value, oldvalue, newvalue, description, icon, className }) {
+    const diff = parseFloat((Math.max(newvalue, oldvalue) / Math.min(newvalue, oldvalue) - 1) *
+        (newvalue > oldvalue ? 100 : -100)).toFixed(2)
+    const valuedesc = (diff < -0.1) ? "decrease"
+        : (diff > -0.1) ? "increase"
+        : "maintain"
+    const colorClass = (diff < 10) ? styles.simple_card_percentage_red
+        : (diff < 0) ? styles.simple_card_percentage_yellow
+        : (diff > 10) ? styles.simple_card_percentage_green
+        : styles.simple_card_percentage_green_light
+    const iconDiff = (diff < 10) ? <BsArrowDown />
+        : (diff < 0) ? <BsArrowDownShort />
+        : (diff > 10) ? <BsArrowUp />
+        : <BsArrowUpShort />
+
+    return <div className={`${styles.simple_card} ${className || ""}`}>
+        <div>
+            <div>
+                {title}
+            </div>
+            <div>
+                <h4>{value}</h4>
+            </div>
+            <div style={{ "display": "flex", "flex-direction": "row", "align-items": "center" }}>
+                <div className={colorClass} style={{ "marginRight": "5px" }}>
+                    <BsArrowUp />
+                    {`${diff}%`}
+                </div>
+                {`${valuedesc} ${description}`}
+            </div>
+        </div>
+        <div className={styles.simple_card_icon}>
+            {icon}
+        </div>
+    </div>
+}
+
 export default function SellerCard({ children, title, ...props }) {
     return <Card {...props} style={{
         "filter": "drop-shadow(0px 2px 10px rgba(0, 0, 0, 0.25))",
