46d41786d67ff5c1114d134c9e2dbe2ba75588f6
Kaixi26 <jorgejg575926@gmail.com>
2022-01-18 12:59:53 +0000

    change default panel

--- app/frontend/pages/seller/index.js
+++ app/frontend/pages/seller/index.js
@@ -6,10 +6,11 @@ import { useState, useEffect } from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import { ResponsivePie } from '@nivo/pie'
+import ResponsivePie from 'components/Seller/ResponsivePie'
 import { ResponsiveBar } from "@nivo/bar";
 import { ResponsiveCalendar } from '@nivo/calendar'
 import Link from "next/link"
+import SellerCard from "components/Seller/Card"
 
 import styles from "styles/Seller/index.module.css"
 import useFetchData from "hooks/useFetchData"
@@ -93,52 +94,9 @@ function OrdersPie({ ordersOverview }) {
     { "id": "processing", "label": "Processing", "value": ordersOverview.processing },
     { "id": "sent", "label": "Processing", "value": ordersOverview.sent },
   ]
-  return <div className={styles.panel}>
-    <h5>Orders State</h5>
-    <div className={styles.panel_orders}>
-    <ResponsivePie
-      data={data}
-      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-      innerRadius={0.5}
-      padAngle={2}
-      cornerRadius={3}
-      activeOuterRadiusOffset={8}
-      borderWidth={1}
-      borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
-      arcLinkLabelsSkipAngle={10}
-      arcLinkLabelsTextColor="#333333"
-      arcLinkLabelsThickness={2}
-      arcLinkLabelsColor={{ from: 'color' }}
-      arcLabelsSkipAngle={10}
-      arcLabelsTextColor={{ from: 'color', modifiers: [['darker', 2]] }}
-      legends={[
-        {
-          anchor: 'bottom',
-          direction: 'row',
-          justify: false,
-          translateX: 0,
-          translateY: 56,
-          itemsSpacing: 0,
-          itemWidth: 100,
-          itemHeight: 18,
-          itemTextColor: '#999',
-          itemDirection: 'left-to-right',
-          itemOpacity: 1,
-          symbolSize: 18,
-          symbolShape: 'circle',
-          effects: [
-            {
-              on: 'hover',
-              style: {
-                itemTextColor: '#000'
-              }
-            }
-          ]
-        }
-      ]}
-    />
-  </div >
-  </div>
+  return <SellerCard className={styles.panel_orders} title={"Orders State"}>
+    <ResponsivePie data={data} />
+  </SellerCard>
 }
 
 
