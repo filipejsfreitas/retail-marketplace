2f09c0aac65b991b3d1cbb06381ed62e086a7caa
Kaixi26 <jorgejg575926@gmail.com>
2022-01-18 12:59:53 +0000

    change default panel

---
+++ app/frontend/components/Seller/Card.js
@@ -0,0 +1,18 @@
+import { Card } from "react-bootstrap"
+
+export default function SellerCard({ children, title, ...props }) {
+    return <Card {...props} style={{
+        "filter": "drop-shadow(0px 2px 10px rgba(0, 0, 0, 0.25))",
+        "border": "1px solid #EAEDF2",
+        "border-radius": "10px",
+    }}>
+        <Card.Header>
+            <h4>
+                {title}
+            </h4>
+        </Card.Header>
+        <Card.Body style={{"width": "100%", "height": "100%"}}>
+            {children}
+        </Card.Body>
+    </Card>
+}
