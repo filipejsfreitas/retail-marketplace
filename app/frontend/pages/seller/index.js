93a7f994589ef6101d7489c80cd2ee99a5de0345
Kaixi26 <jorgejg575926@gmail.com>
2022-01-19 15:33:04 +0000

    [Frontend] - separate recommended categories request

--- app/frontend/pages/seller/index.js
+++ app/frontend/pages/seller/index.js
@@ -13,39 +13,30 @@ import YearRevenue from "components/Seller/Card/YearRevenue"
 import { SimpleCard } from "components/Seller/Card"
 
 import { BsTruck, BsBank, BsBoxSeam } from "react-icons/bs"
-import useFetchData from "hooks/useFetchData"
 import usePanelOrdersOverview from "hooks/Seller/usePanelOrdersOverview"
 import usePanelRevenueOverview from "hooks/Seller/usePanelRevenueOverview"
 import usePanelAlerts from "hooks/Seller/usePanelAlerts"
+import usePanelRecommendedCategories from "hooks/Seller/usePanelRecommendedCategories"
 
 import styles from "styles/Seller/index.module.css"
 
 export default function Home() {
-  const { fetchAuth: fetch } = useFetchAuth()
-  const { isReady } = useRouter()
-  const [panel, setPanel] = useState(undefined)
-
-  useEffect(() => {
-    if (!isReady) return
-    fetch(`${process.env.NEXT_PUBLIC_HOST}/sellerPanel`)
-      .then(rep => rep.json())
-      .then(p => setPanel(p))
-  }, [isReady])
 
   const { ordersOverview, loading: loadingOrdersOverview } = usePanelOrdersOverview()
   const { revenueOverview, loading: loadingRevenueOverview } = usePanelRevenueOverview()
   const { lowStockProposals, loading: loadingAlerts } = usePanelAlerts()
+  const { recommendedCategories, loading: loadingRecommendedCategories } = usePanelRecommendedCategories()
 
   return <Layout sidebar={SELLER_SIDEBAR}>
     <div className={styles.content}>
-      <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={30} newvalue={40} description={"this week"} className={styles.simple_panel} icon={<BsTruck/>}/>
-      <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={40} newvalue={30} description={"this week"} className={styles.simple_panel} icon={<BsBank/>}/>
-      <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={31} newvalue={30} description={"this week"} className={styles.simple_panel} icon={<BsBoxSeam/>}/>
-      <DailyRevenue revenueOverview={revenueOverview} loading={loadingRevenueOverview}/>
-      <YearRevenue revenueOverview={revenueOverview} loading={loadingRevenueOverview}/>
+      <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={30} newvalue={40} description={"this week"} className={styles.simple_panel} icon={<BsTruck />} />
+      <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={40} newvalue={30} description={"this week"} className={styles.simple_panel} icon={<BsBank />} />
+      <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={31} newvalue={30} description={"this week"} className={styles.simple_panel} icon={<BsBoxSeam />} />
+      <DailyRevenue revenueOverview={revenueOverview} loading={loadingRevenueOverview} />
+      <YearRevenue revenueOverview={revenueOverview} loading={loadingRevenueOverview} />
       <OrdersPie ordersOverview={ordersOverview} loading={loadingOrdersOverview} />
-      <LowStockProposals lowStockProposals={lowStockProposals} loading={loadingAlerts}/>
-      <RecommendedCategories recommendedCategories={panel ? panel.recommendedCategories.categories : undefined} loading={panel === undefined}/>
+      <LowStockProposals lowStockProposals={lowStockProposals} loading={loadingAlerts} />
+      <RecommendedCategories recommendedCategories={recommendedCategories} loading={loadingRecommendedCategories} />
     </div>
   </Layout>
 }
