fb83ef6a5ef6d606351ea158a6fef950bd8cf563
Kaixi26 <jorgejg575926@gmail.com>
2022-01-19 14:09:54 +0000

    [Frontend] - Card bugfix

--- app/frontend/pages/seller/index.js
+++ app/frontend/pages/seller/index.js
@@ -41,7 +41,7 @@ export default function Home() {
     <div className={styles.content}>
       <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={30} newvalue={40} description={"this week"} className={styles.simple_panel} icon={<BsTruck/>}/>
       <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={40} newvalue={30} description={"this week"} className={styles.simple_panel} icon={<BsBank/>}/>
-      <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={33} newvalue={30} description={"this week"} className={styles.simple_panel} icon={<BsBoxSeam/>}/>
+      <SimpleCard title={"SALES"} value={"30.234€"} oldvalue={31} newvalue={30} description={"this week"} className={styles.simple_panel} icon={<BsBoxSeam/>}/>
       <DailyRevenue revenueOverview={revenueOverview} loading={loadingRevenueOverview}/>
       <YearRevenue revenueOverview={revenueOverview} loading={loadingRevenueOverview}/>
       <OrdersPie ordersOverview={ordersOverview} loading={loadingOrdersOverview} />
