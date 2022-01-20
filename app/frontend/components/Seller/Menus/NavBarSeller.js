ce3426d01ac1e8c75a0f2d8e78f56101a1eba84e
Kaixi26 <jorgejg575926@gmail.com>
2022-01-20 16:52:01 +0000

    [Frontend] - Instantly load side and top bar

--- app/frontend/components/Seller/Menus/NavBarSeller.js
+++ app/frontend/components/Seller/Menus/NavBarSeller.js
@@ -21,33 +21,34 @@ export default function NavBarSeller(props) {
      useFetchData(() => `${process.env.NEXT_PUBLIC_HOST}/seller/${token._id}`,
         {  when:token && token._id})
 
-    return(loading ? <Spinner animation="border" /> :
-        <>
+    return <>
             <Navbar className={styles.navbar} bg="primary" >
                 <Container fluid>
                     <div className={styles.title}>Dashboard</div>
                     <Row md={12} className={styles.outCont}>
+                    {loading || !seller ? <Col><Spinner style={{"color": "white"}} animation="border" /></Col> : <>
                         <Col className={styles.gearCont}>
                             <button type="submit" className={styles.btn}
                                 onClick={() => setModalShow(true)}>
-                                <BsGearFill size={45} className={styles.gear}/>
+                                <BsGearFill size={45} className={styles.gear} />
                             </button>
                             <EditSeller
                                 show={modalShow}
                                 onHide={() => setModalShow(false)}
                                 seller={seller}
-                        />
+                            />
                         </Col>
                         <Col className={styles.infoSeller}>
-                        {userType == UserType.SELLER ?
-                        <button ref={dropButtonRef} className={styles.btn}
-                          onClick={() => setshowDropdown(!showDropdown)}
-                        >
-                            <BsPersonCircle size={48} className={styles.circle} />
-                            <div  className={styles.name}>{(token.sellerInfo).firstName} {(token.sellerInfo).lastName}</div>
-                        </button>
-                        : <></>}
+                            {userType == UserType.SELLER ?
+                                <button ref={dropButtonRef} className={styles.btn}
+                                    onClick={() => setshowDropdown(!showDropdown)}
+                                >
+                                    <BsPersonCircle size={48} className={styles.circle} />
+                                    <div className={styles.name}>{seller.firstName} {seller.lastName}</div>
+                                </button>
+                                : <></>}
                         </Col>
+                    </>}
                     </Row>
                 </Container>
             </Navbar>
@@ -56,5 +57,4 @@ export default function NavBarSeller(props) {
               state={[showDropdown, setshowDropdown]}
             />
         </>
-    )
 }
