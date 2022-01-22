0529d5a3b59093c1ee46b36be80582eefd4eab79
Maria <maria.a.b20000@hotmail.com>
2022-01-22 20:37:51 +0000

    correção nome base de dados e coleções

--- Povoamento/populate.py
+++ Povoamento/populate.py
@@ -3,13 +3,13 @@ import signal
 import subprocess
 
 
-s1 = subprocess.Popen(['mongoimport', '--db', 'retailMarketplace', '-c','categorie', 'category.json', '--jsonArray', '--upsert','--drop'])
-s2 = subprocess.Popen(['mongoimport', '--db', 'retailMarketplace', '-c','user', 'users.json', '--jsonArray', '--upsert','--drop'])
-s3 = subprocess.Popen(['mongoimport', '--db', 'retailMarketplace', '-c','seller', 'sellers.json', '--jsonArray', '--upsert','--drop'])
-s4 = subprocess.Popen(['mongoimport', '--db', 'retailMarketplace', '-c','client', 'clients.json', '--jsonArray', '--upsert','--drop'])
-s5 = subprocess.Popen(['mongoimport', '--db', 'retailMarketplace', '-c','product', 'products.json', '--jsonArray', '--upsert','--drop'])
-s6 = subprocess.Popen(['mongoimport', '--db', 'retailMarketplace', '-c','sellerComment', 'sellerComments.json', '--jsonArray', '--upsert','--drop'])
-s7 = subprocess.Popen(['mongoimport', '--db', 'retailMarketplace', '-c','proposal', 'proposals.json', '--jsonArray', '--upsert','--drop'])
+s1 = subprocess.Popen(['mongoimport', '--db', 'retailmarketplace', '-c','categories', 'category.json', '--jsonArray', '--upsert','--drop'])
+s2 = subprocess.Popen(['mongoimport', '--db', 'retailmarketplace', '-c','users', 'users.json', '--jsonArray', '--upsert','--drop'])
+s3 = subprocess.Popen(['mongoimport', '--db', 'retailmarketplace', '-c','sellers', 'sellers.json', '--jsonArray', '--upsert','--drop'])
+s4 = subprocess.Popen(['mongoimport', '--db', 'retailmarketplace', '-c','clients', 'clients.json', '--jsonArray', '--upsert','--drop'])
+s5 = subprocess.Popen(['mongoimport', '--db', 'retailmarketplace', '-c','products', 'products.json', '--jsonArray', '--upsert','--drop'])
+s6 = subprocess.Popen(['mongoimport', '--db', 'retailmarketplace', '-c','sellerComments', 'sellerComments.json', '--jsonArray', '--upsert','--drop'])
+s7 = subprocess.Popen(['mongoimport', '--db', 'retailmarketplace', '-c','proposals', 'proposals.json', '--jsonArray', '--upsert','--drop'])
 
 
 
