c0dc5dc6694a1db4781b4caf11f1ba4ac23ed3a1
Kaixi26 <jorgejg575926@gmail.com>
2022-01-23 13:32:30 +0000

    [Populate] - add requirements

--- app/populate/main.py
+++ app/populate/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 from pymongo import MongoClient
 from logger import Logger
 from clients import *
@@ -10,31 +11,44 @@ from comments import *
 from addresses import *
 from orders import *
 
-class State():
-    baseurl = "http://localhost:3001"
-    mongo = MongoClient("localhost", 27017)["retail"]
-    logger = Logger()
-    clients = clients
-    sellers = sellers
-    categories = categories
-    products = products
-    proposals = []
-    comments = comments
-    addresses = []
-    orders = []
 
-state = State()
+def populate(state):
+    add_clients(state)
+    populate_addresses(state)
+    add_addresses(state)
+    add_sellers(state)
+    add_categories(state)
+    populate_proposals(state)
+    add_proposals(state)
+    populate_comments(state)
+    add_comments(state)
+    populate_orders(state)
+    add_orders(state)
 
-add_clients(state)
-populate_addresses(state)
-add_addresses(state)
-add_sellers(state)
-add_categories(state)
-populate_proposals(state)
-add_proposals(state)
-populate_comments(state)
-add_comments(state)
-populate_orders(state)
-add_orders(state)
 
-state.logger.info("ended :)")
+program = sys.argv.pop(0)
+
+if len(sys.argv) == 0 or len(sys.argv) != 2:
+    print("The database should be empty when populating prevent errors.")
+    print("To remove the database you can use the following commands:")
+    print("  mongo")
+    print("  > use dbname")
+    print("  > db.dropDatabase()")
+    print("To populate use '" + program +
+          " --db [dbname]'. (Both servers must be running)")
+elif sys.argv.pop(0) == "--db" and len(sys.argv) == 1:
+    class State():
+        mongo = MongoClient("localhost", 27017)[sys.argv.pop(0)]
+        logger = Logger()
+        clients = clients
+        sellers = sellers
+        categories = categories
+        products = products
+        proposals = []
+        comments = comments
+        addresses = []
+        orders = []
+    state = State()
+
+    populate(state)
+    state.logger.info("ended :)")
